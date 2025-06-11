00deb3a61b2cbb40b572128137daf3efbba1d183
Shyaman Jayasundara <shayaman321@gmail.com>
2025-06-11 14:07:50 -0400

    Update gseaplot.R

--- R/gseaplot.R
+++ R/gseaplot.R
@@ -304,7 +304,7 @@ gseaplot2 <- function(x, geneSetID, title = "", color="green", base_size = 11,
     }
 
     if (pvalue_table) {
-        pd <- x[geneSetID, c("Description", "pvalue", "p.adjust")]
+        pd <- x[geneSetID, c("Description", "NES", "p.adjust")]
         # pd <- pd[order(pd[,1], decreasing=FALSE),]
         rownames(pd) <- pd$Description
 
